349c3e338280747200a94455b853ae2a7cd40837
Mohit Agarwal <mohitagarwal@MacShik4ever.local>
2018-06-18 01:47:47 -0700

    io emit

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,26 @@
 var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createEmail', {
-    to: 'mohit@gmail',
-    text: 'This is an email'
-  });
-
-  socket.emit('createMessage', {
-    to: 'mohit@gmail',
-    text: 'This is a message'
-  });
-
 });
+  // socket.emit('createEmail', {
+  //   to: 'mohit@gmail',
+  //   text: 'This is an email'
+  // });
+
+//   socket.emit('createMessage', {
+//     from: 'mohit@gmail',
+//     text: 'This is a message'
+//   });
+//
+// });
 
 socket.on('newMessage', (newMsg) => {
   console.log('Got new message', newMsg);
 });
 
-socket.on('newEmail', function(email) {
-  console.log('New email', email);
-});
+// socket.on('newEmail', function(email) {
+//   console.log('New email', email);
+// });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
